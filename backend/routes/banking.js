1933c1e78ede4aa1d6f180ced4c1e64957d08381
Dylan Barks <91198979+dbarks508@users.noreply.github.com>
2025-10-29 09:29:15 -0600

    setting up deposit route

--- backend/routes/banking.js
+++ backend/routes/banking.js
@@ -1,7 +1,32 @@
 // backend banking routes
 
 const express = require("express");
-const userRoutes = express.Router();
+const transactionRoutes = express.Router();
 const dbo = require("../db/conn");
 
-// TODO everything
+// deposit route
+transactionRoutes.route("/deposit").post(async (req, res) => {
+  try {
+    // destructure req from front end
+    const { accountNumber, accountName, amount } = req.body;
+
+    // connect db and query for the user record
+    let db = dbo.getDB();
+    const usersCollection = db.collection("users");
+    const user = await usersCollection.findOne({
+      accountNumber: accountNumber,
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ error: "no user with such accountNumber found" });
+    }
+
+    // deposit amount into selected account
+    //TODO
+  } catch (error) {
+    console.log("in route /depost catch error block");
+    throw err;
+  }
+});
